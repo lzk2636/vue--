cc865fc04fd8bdfaf1a8cad309dc97882cf82c9a
lzk2636 <2820421644@qq.com>
2019-12-12 14:43:07 +0800

    整合了element-ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 // 导入 vue-router
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import App from '@/components/HelloWorld.vue'
 //注册 vue-router
 Vue.use(VueRouter)
 // 实例化
@@ -8,8 +9,8 @@ const router = new VueRouter({
     //这里就是路由的配制项
     routes: [
         {
-            path: '/这里可给路由地址一个名字',
-            component: '/home'
+            path: '/',
+            component: App
         }
     ]
 })
